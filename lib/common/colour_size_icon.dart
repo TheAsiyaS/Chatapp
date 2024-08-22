e4c82aea10c471c07c95b09c5112dc247a9c845c
TheAsiyaS <theasiya.s@gmail.com>
2024-08-22 21:30:09 +0530

    necessary colour,size,icon are added

---
+++ lib/common/colour_size_icon.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+const kblack = Colors.black;
+const kwhite = Colors.white;
+const klightwhite = Color.fromARGB(164, 255, 255, 255);
+const kblue = Colors.blue;
+const kgrey = Colors.grey;
+const kgreyTransparent = Color.fromARGB(134, 70, 70, 70);
+const kblackTransparent = Color.fromARGB(160, 38, 38, 38);
+const kRed = Colors.red;
+const ktransparent = Colors.transparent;
+const kblackdarktrans = Color.fromARGB(194, 0, 0, 0);
+const kamber = Colors.amber;
+
+const w2 = SizedBox(
+  width: 2,
+);
+const w10 = SizedBox(
+  width: 10,
+);
+const w20 = SizedBox(
+  width: 20,
+);
+const w30 = SizedBox(
+  width: 30,
+);
+const w40 = SizedBox(
+  width: 40,
+);
+const w50 = SizedBox(
+  width: 50,
+);
+const h10 = SizedBox(
+  height: 10,
+);
+const h20 = SizedBox(
+  height: 20,
+);
+const h30 = SizedBox(
+  height: 30,
+);
+const h40 = SizedBox(
+  height: 40,
+);
+const h50 = SizedBox(
+  height: 50,
+);
+const kadd = Icons.add;
+const kiconsearch = Icons.search;
+const knotification = Icons.notifications_none_outlined;
+const kfavorite = Icons.favorite;
+const kfavoriteOutline = Icons.favorite_border_outlined;
+const kclose = Icons.close;
+const kmoreVertical = Icons.more_vert_outlined;
+const kcheck = CupertinoIcons.check_mark;
+const kshare = Icons.share_outlined;
+const kgoback = Icons.arrow_back;
+const kmenu = Icons.menu;
+const ksqGrid = CupertinoIcons.square_grid_2x2_fill;
+const kstars = CupertinoIcons.star;
+const kstarsfilled = CupertinoIcons.star_fill;
+const kLocation = Icons.location_on;
+const klistdash = CupertinoIcons.list_dash;
+const ksave = Icons.bookmark_border;
+const ksaved = Icons.bookmark;
+const kforward = CupertinoIcons.chevron_right_circle_fill;
+const kbackward = CupertinoIcons.chevron_left_circle_fill;
+const kdowncirclefilled = CupertinoIcons.chevron_down_circle_fill;
+const kDown = Icons.arrow_drop_down_rounded;
+const kcall = Icons.phone;
+const kmail = CupertinoIcons.mail;
+const kaddRound = Icons.add_circle;
+const ksetting = Icons.settings;
+const kperson = Icons.person_outline_outlined;
+const knote = CupertinoIcons.news;
+const kinfoOutline = Icons.info_outline;
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:message_app/common/colour_size_icon.dart';
+import 'package:message_app/presentation/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          scaffoldBackgroundColor: kblack,
+          brightness: Brightness.dark,
+        ),
+        home: const Home());
+  }
+}
